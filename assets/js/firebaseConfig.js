eb93952a45397dc79477b0cee161faf0c9c9a5ff
Omar Imam <admin@chip.local>
2020-05-29 15:58:33 -0500

    Added Firebase Configuration

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyDgb53eA7xY_or8Lbr5KrFyEAcweqGTh2E',
+  authDomain: 'cleansweep-6eedd.firebaseapp.com',
+  databaseURL: 'https://cleansweep-6eedd.firebaseio.com',
+  projectId: 'cleansweep-6eedd',
+  storageBucket: 'cleansweep-6eedd.appspot.com',
+  messagingSenderId: '731187500214',
+  appId: '1:731187500214:web:9a3e123dad1eaa096d8217',
+  measurementId: 'G-93JJ383TPN'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
