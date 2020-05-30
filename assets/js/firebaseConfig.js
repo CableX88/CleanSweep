e085ba4c17d1fbbd51f9f641c4604b28b6fde4e0
CableX88 <Nullspace101@gmail.com>
2020-05-29 21:46:46 -0700

    Modified Config to include authentication code

--- assets/js/firebaseConfig.js
+++ assets/js/firebaseConfig.js
@@ -12,3 +12,53 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+// form elements
+const TextEmail = document.getElementById("TextEmail");
+const TextPassword = document.getElementById("TextPassword");
+const LoginButton = document.getElementById("LoginButton");
+const SignUpButton = document.getElementById("SignUpButton");
+const LogoutButton = document.getElementById("LogoutButton");
+
+//Login in event
+
+LoginButton.addEventListener('click', e => {
+  //get email and password
+  const email = TextEmail.value;
+  const pword = TextPassword.value;
+  const auth = firebase.auth();
+  //Sign in
+  const promise = auth.sighInWithEmailAndPassword(email,pword);
+  promise.catch(e => console.log(e.message));
+
+});
+//sigh up event
+SignUpButton.addEventListener('click',e =>{
+   //get email and password
+   const email = TextEmail.value;
+   const pword = TextPassword.value;
+   const auth = firebase.auth();
+   //Sign up
+   const promise = auth.createUserWithEmailAndPassword(email,pword);
+   promise.catch(e => console.log(e.message));
+
+});
+
+
+LogoutButton.addEventListener('click', e =>{
+  firebase.auth().sighOut();
+});
+
+//Real time listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+    console.log(firebaseUser);
+    LogoutButton.classList.remove('hide');
+  }else{
+    console.log('Error: User not logged in');
+    LogoutButton.classList.add('hide');
+  }
+
+});
+
+
